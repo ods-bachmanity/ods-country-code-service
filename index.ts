a80bec1d57181e4523246a43800ce82d0acc70af
Steven Sederburg <stevens@blackspoke.com>
2018-11-30 11:50:59 -0600

    moved appdynamics code into common kyber-server library

--- index.ts
+++ index.ts
@@ -1,24 +1,7 @@
-const env = require("dotenv").config()
-if (process.env.APPDYNAMICS_HOSTNAME) {
-    require('appdynamics').profile({
-        controllerHostName: process.env.APPDYNAMICS_HOSTNAME,
-        controllerPort: process.env.APPDYNAMICS_PORT, 
-        
-        // If SSL, be sure to enable the next line
-        controllerSslEnabled: process.env.APPDYNAMICS_SSLENABLED,
-        accountName: process.env.APPDYNAMICS_ACCOUNTNAME,
-        accountAccessKey: process.env.APPDYNAMICS_ACCOUNTKEY,
-        applicationName: process.env.APPDYNAMICS_APPNAME,
-        tierName: process.env.APPDYNAMICS_TIERNAME,
-        nodeName: process.env.APPDYNAMICS_NODENAME // The controller will automatically append the node name with a unique number
-    })
-}
-
 import {KyberServer, KyberServerEvents } from 'kyber-server'
 import * as config from 'config'
 import { HealthCheckGetSchematic, GetCountriesSchematic, PostCountriesSchematic, CountryCodeServiceSchematic } from './schematics'
 import { DataProvider, Logger } from './common'
-
 
 const kyber = new KyberServer({
     port: config.port

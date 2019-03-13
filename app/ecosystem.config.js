ee4e10df5a9399d7edf80d496a54eb5775dea404
Steven Sederburg <stevens@blackspoke.com>
2019-03-13 09:10:33 -0500

    sync master and dev branches

--- app/ecosystem.config.js
+++ app/ecosystem.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-    apps : [{
-      name: "ccs",
-      script: "./index.js",
-      instances: -1,
-      env: {
-        NODE_ENV: "production"
-      }
-    }]
-  }
+  apps : [{
+    name: "ccs",
+    script: "./index.js",
+    instances: -1,
+    log: "/var/log/country-code-service/app/app.log",
+    merge_logs: true,
+    log_type: "json",
+    env: {
+      NODE_ENV: "production"
+    }
+  }]
+}
